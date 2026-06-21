19eb9b7bbf2b7e73d83c2dad0c717fed1ebd9455
tuscan-chicken-wrap <boan.wang@gmail.com>
2026-06-20 18:04:35 -0700

    Updating survey

--- survey/start_server.sh
+++
@@ -1,4 +0,0 @@
-source ../venv/bin/activate
-nohup zrok share public 9890 --headless &> zrok_output.log &
-nohup python survey/server.py   --k 25   --pipeline-output-dir runs/context_pipeline_generation
- &> output.log &
